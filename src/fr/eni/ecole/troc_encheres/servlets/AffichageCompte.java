1eb8deb0132a6c4975d3c8540e299fa0f7ce5b57
Dominika <dominika.sliwa@gmail.com>
2020-05-20 12:13:59 +0200

    affichage profil

--- src/fr/eni/ecole/troc_encheres/servlets/AffichageCompte.java
+++ src/fr/eni/ecole/troc_encheres/servlets/AffichageCompte.java
@@ -6,16 +6,32 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+import fr.eni.ecole.troc_encheres.bll.EncheresManager;
+import fr.eni.ecole.troc_encheres.bll.exceptions.BLLException;
+import fr.eni.ecole.troc_encheres.bo.Utilisateur;
 
 /**
  * Servlet implementation class AffichageCompte
- * @author Lucille
+ * @author Lucille/Dominika
  */
 @WebServlet("/AffichageCompte")
 public class AffichageCompte extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static final String VUE = "/WEB-INF/jsp/affichage_compte.jsp";
+	private EncheresManager manager;
+	int noUtilisateur =-1;
+	Utilisateur util = null;
 
+	@Override
+	public void init() throws ServletException {
+		manager = EncheresManager.get();
+	}
+	
+	
+	
 	/**
 	 * @see HttpServlet#HttpServlet()
 	 */
@@ -31,8 +47,38 @@ public class AffichageCompte extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
+		// verifier si la session existe
+		HttpSession session = request.getSession();
+		if (session.getAttribute("sessionUtilisateur") == null) {
+			response.sendRedirect(getServletContext().getContextPath() + "/connexion");
+		}else {
+			noUtilisateur = ((Utilisateur) session.getAttribute("sessionUtilisateur")).getNumero();
+
+
+			try {
+				util = manager.getUtil(noUtilisateur);
+				
+				session.setAttribute("pseudo", util.getPseudo());
+				session.setAttribute("nom", util.getNom());
+				session.setAttribute("prenom", util.getPrenom());
+				session.setAttribute("email", util.getEmail());
+				session.setAttribute("tel", util.getTel());
+				session.setAttribute("rue", util.getRue());
+				session.setAttribute("cp", util.getCp());
+				session.setAttribute("ville", util.getVille());
+				session.setAttribute("mdp", util.getMdp());
+				session.setAttribute("pseudo", util.getPseudo());
+				request.getRequestDispatcher(VUE).forward(request, response);
+				
+			} catch (BLLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+
+		}
 	}
+
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
